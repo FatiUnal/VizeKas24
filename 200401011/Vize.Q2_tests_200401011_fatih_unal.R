cdecf15119baa49a6d9d3eecb878de2f3872a395
Fatih <fatih_0226@outlook.com>
2023-11-24 16:52:26 +0300

    TEST 2.2 tamamlandı

--- 200401011/Vize.Q2_tests_200401011_fatih_unal.R
+++ 200401011/Vize.Q2_tests_200401011_fatih_unal.R
@@ -2,7 +2,12 @@ library(testthat)
 rm(list = ls())
 source("./200401011/Vize_Q2_200401011_fatih_unal.R")
 
-# T 2-1
+# 2.1
 test_that("Global Workspace’de spotify_search_artist adlı bir değişken olmalı.", {
   expect_true(exists("spotify_search_artist"))
+})
+
+#2.2
+test_that("spotify_search_artist adlı değişkenin tipi “function” olmalı.", {
+  expect_true(is.function(spotify_search_artist))
 })
